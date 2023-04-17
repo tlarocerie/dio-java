8c75247ea1c32140c8e32cf61338119c3100d4f6
tlarocerie <thomaz.magalha@gmail.com>
2023-04-17 00:27:49 -0300

    operador ternário

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,12 @@
+public class OperadorTernario {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+    
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+    }
+
+}
