fbf3477477ad7c39622ec25e7de1c600fba3cb72
tlarocerie <thomaz.magalha@gmail.com>
2023-04-19 00:36:46 -0300

    FInal

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500;
+        
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        System.out.print(numeroCurto2);
     }
 }
